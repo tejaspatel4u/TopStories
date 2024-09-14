2a6189ae8105564ee08ddace0ae84fefd0ddb694
Tejas Patel <tejas.patel4u@gmail.com>
2024-09-14 18:07:41 +0530

    Commit 14-09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
-
+import { multicast } from 'rxjs/operators';
+import { APIService } from './api.service';
+import { HttpProviderService } from './http-provider.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'topstories';
-  APIKey = "9ec6f98c-8248-403b-aba5-3cc6eb79598b";
 
-  getStories(){
-    
-    //this.APIKey="check";
+  title = 'Top Stories';
+  APIKey = "9ec6f98c-8248-403b-aba5-3cc6eb79598b";
+  stories: any;
+
+  constructor(private toastr: ToastrService, private httpProviderService: HttpProviderService) { }
+
+  getStories() {
+    this.httpProviderService.getAllStories(this.APIKey).subscribe((data: any) => {
+      this.stories = data;
+      console.log(this.stories.body);
+    },
+      err => {
+        if (err.status === 401) {
+          this.toastr.error('Unauthorized Access');
+        }
+        else
+          this.toastr.error(err);
+      });
+  }
+
+  getImage(Multimedia: any[]) {
+    if (Multimedia) {
+      return Multimedia[0].url;
+    }
+
+    return '';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,10 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
